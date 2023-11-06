1f14bcf3dd0f98012716b2a5ba83ba234467f9a7
andersonlouzada <anderson.s.louzada@gmail.com>
2023-11-06 11:49:41 -0300

    test: create test to priorityQueue

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,21 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priorityQueue = PriorityQueue()
+    items = [1, 2, 3, 4, 5]
+
+    for item in items:
+        priorityQueue.enqueue({"qtd_linhas": item})
+
+    assert len(priorityQueue) == 5
+    assert priorityQueue.high_priority.data.head.item["qtd_linhas"] == 5
+
+    priorityQueue.dequeue()
+    data = priorityQueue.search(1)
+
+    assert data["qtd_linhas"] == 4
+
+    with pytest.raises(IndexError):
+        priorityQueue.search(10)
