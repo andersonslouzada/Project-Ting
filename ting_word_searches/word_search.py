b07d62e9ff5c14bed5267f0b7bcaa32968079482
andersonlouzada <anderson.s.louzada@gmail.com>
2023-11-06 13:04:23 -0300

    feat: create function search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -18,4 +18,19 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        data = instance.search(i)
+        name = data['nome_do_arquivo']
+        ocurrences = []
+
+        for number, line in enumerate(data['linhas_do_arquivo'], start=1):
+            if word.lower() in line.lower():
+                ocurrences.append({'linha': number, 'conteudo': line})
+
+        if ocurrences:
+            result.append({'palavra': word,
+                           'arquivo': name,
+                           'ocorrencias': ocurrences})
+
+    return result
