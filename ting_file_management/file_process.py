0a772f13c410910173844ab87ee24853e1ea3e0a
andersonlouzada <anderson.s.louzada@gmail.com>
2023-11-06 11:04:22 -0300

    feat: create function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in instance._data:
+        if i['nome_do_arquivo'] == path_file:
+            sys.stderr.write(f'O arquivo {path_file}'
+                             'já foi processado anteriormente.\n')
+            return
+
+    archive = txt_importer(path_file)
+
+    if archive is None:
+        return
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(archive),
+        'linhas_do_arquivo': archive
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
