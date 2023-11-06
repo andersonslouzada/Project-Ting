b6c0fcb419b335825e36f5d526e715f539c41ce5
andersonlouzada <anderson.s.louzada@gmail.com>
2023-11-06 12:03:39 -0300

    fix: change back test to initial

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,21 +1,5 @@
-import pytest
-from ting_file_management.priority_queue import PriorityQueue
+# from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    priorityQueue = PriorityQueue()
-    items = [1, 2, 3, 4, 5]
-
-    for item in items:
-        priorityQueue.enqueue({"qtd_linhas": item})
-
-    assert len(priorityQueue) == 5
-    assert priorityQueue.high_priority.data.head.item["qtd_linhas"] == 5
-
-    priorityQueue.dequeue()
-    data = priorityQueue.search(1)
-
-    assert data["qtd_linhas"] == 4
-
-    with pytest.raises(IndexError):
-        priorityQueue.search(10)
+    """Aqui irá sua implementação"""
